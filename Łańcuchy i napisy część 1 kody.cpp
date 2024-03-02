d9b9f7cb3aec3eca346d9f46119ada0ae4b820dd
TomJonEZN <161930673+TomJonEZN@users.noreply.github.com>
2024-03-02 20:30:56 +0100

    Update Łańcuchy i napisy część 1 kody.cpp

--- Łańcuchy i napisy część 1 kody.cpp
+++ Łańcuchy i napisy część 1 kody.cpp
@@ -1,1 +1,12 @@
+//tworzenie napisów
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string imie = "Marceli";
+  string nazwisko("Debeściak");
+  cout << imie << " " << nazwisko << endl;
+  return 0;
+}
